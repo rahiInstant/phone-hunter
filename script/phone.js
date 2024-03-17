5be49ca0d76195b244e9f7d63c603c167c1b8b4e
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-03-17 16:57:58 +0600

    add all functionality of search

--- script/phone.js
+++ script/phone.js
@@ -1,8 +1,9 @@
 var items;
 const showAllBtn = document.getElementById("show-all-btn");
 const showLessBtn = document.getElementById("show-less-btn");
+const modal = document.getElementById('modal')
 
-const loadPhoneData = async (brand = "iphone") => {
+const loadPhoneData = async (brand = "iphone", isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${brand}`
   );
@@ -10,30 +11,37 @@ const loadPhoneData = async (brand = "iphone") => {
   globalThis.items = data.data;
   const phones = data.data;
   document.getElementById("result").innerText = `${items.length} items shows.`;
-  displayPhone(phones);
+  displayPhone(phones, isShowAll);
 };
-function handleShowAllBtn(length = 6) {
-  if (length > 5) {
-    showAllBtn.classList.remove("hidden");
-  } else {
-    showAllBtn.classList.add("hidden");
-  }
-}
+// function handleShowAllBtn(length = 6) {
 
-function handleShowLessBtn(length = 6) {
-  if (length > 5) {
+// }
+
+// function handleShowLessBtn(length = 6) {
+//   if (length > 5) {
+//     showLessBtn.classList.add("hidden");
+//   } else {
+//     showLessBtn.classList.remove("hidden");
+//   }
+// }
+
+const displayPhone = (phones, isShowAll = false) => {
+  const phoneContainer = document.getElementById("phone-container");
+  // handleShowAllBtn(length);
+  // handleShowLessBtn(length);
+
+  if (phones.length > 5 && !isShowAll) {
+    showAllBtn.classList.remove("hidden");
     showLessBtn.classList.add("hidden");
   } else {
+    showAllBtn.classList.add("hidden");
     showLessBtn.classList.remove("hidden");
   }
-}
-
-const displayPhone = (phones, lastIndex = 5, length) => {
-  const phoneContainer = document.getElementById("phone-container");
-  handleShowAllBtn(length);
-  handleShowLessBtn(length);
+  console.log(isShowAll);
   phoneContainer.textContent = "";
-  phones = phones.slice(0, lastIndex);
+  if (!isShowAll) {
+    phones = phones.slice(0, 5);
+  }
   phones.forEach((phone) => {
     const phoneCard = document.createElement("div");
     phoneCard.classList = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 max-w-7xl mx-auto`;
@@ -47,26 +55,66 @@ const displayPhone = (phones, lastIndex = 5, length) => {
           majority have suffered
         </p>
         <h2 class="font-bold text-2xl mt-2">$999</h2>
-        <button class=" btn text-white bg-blue-700 mt-4 hover:text-blue-700 hover:border hover:border-blue-700">Show Details</button>
+        <button onclick="handleDetailBtn('${phone.slug}')" class=" btn text-white bg-blue-700 mt-4 hover:text-blue-700 hover:border hover:border-blue-700">Show Details</button>
       </div>
     </div>
     `;
     // console.log(phoneCard)
     phoneContainer.appendChild(phoneCard);
   });
+  setTimeout(() => {
+    handleLoader(false);
+  }, 1000);
 };
 
 loadPhoneData();
 
-document.getElementById("hunterBtn").addEventListener("click", function () {
-  const brand = document.getElementById("hunterField").value;
-  loadPhoneData(brand);
-});
+function handleSearch(isShowAll) {
+  handleLoader(true);
+  const brand = document.getElementById("hunterField");
+  let brandName = brand.value == "" ? "iphone" : brand.value;
+  // let brandName = brand.value;
+  loadPhoneData(brandName, isShowAll);
+  // brand.value = ''
+}
+
+
+// showAllBtn.onclick = function () {
+//   displayPhone(items, items.length, 5);
+// };
+
+// showLessBtn.onclick = function () {
+//   displayPhone(items, 5, 6);
+// };
 
 showAllBtn.onclick = function () {
-  displayPhone(items, items.length, 5);
+  handleSearch(true);
 };
 
-showLessBtn.onclick = function () {
-  displayPhone(items, 5, 6);
-};
+showLessBtn.onclick = function() {
+  handleSearch(false)
+}
+
+
+function handleLoader(isLoading) {
+  const loader = document.getElementById("loader").classList;
+  if (isLoading) {
+    loader.remove("hidden");
+  } else {
+    loader.add("hidden");
+  }
+}
+
+
+function handleDetailBtn(id) {
+  console.log('detail')
+  console.log(id)
+  // document.getElementById('img').
+  modal.classList.remove('opacity-0')
+  modal.classList.remove('pointer-events-none')
+}
+
+function handleContinueBtn() {
+  modal.classList.add('opacity-0')
+  modal.classList.add('pointer-events-none')
+}
